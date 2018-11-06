c829fe1e139354bf021226bab544f1b4a673cc27
GedMarc <ged_marc@hotmail.com>
2018-11-06 21:29:25 +0200

    JRE 11 full split and module specifications a bit stricter

---
+++ src/main/java/com/jwebmp/plugins/moment/implementations/MomentExclusionsModule.java
@@ -0,0 +1,30 @@
+package com.jwebmp.plugins.moment.implementations;
+
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
+
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+public class MomentExclusionsModule
+		implements IGuiceScanModuleExclusions<MomentExclusionsModule>,
+				           IGuiceScanJarExclusions<MomentExclusionsModule>
+{
+
+	@Override
+	public @NotNull Set<String> excludeJars()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("jwebmp-momentjs-*");
+		return strings;
+	}
+
+	@Override
+	public @NotNull Set<String> excludeModules()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("com.jwebmp.plugins.moment");
+		return strings;
+	}
+}
