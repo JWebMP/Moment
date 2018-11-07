f371310cb40eb90457ad6d732ad1ea4b2cbcfecd
GedMarc <ged_marc@hotmail.com>
2018-11-07 03:35:19 +0200

    Deeper enablement support

--- src/main/java/com/jwebmp/plugins/moment/MomentAngularModule.java
+++ src/main/java/com/jwebmp/plugins/moment/MomentAngularModule.java
@@ -45,4 +45,15 @@ public class MomentAngularModule
 	{
 		return "";
 	}
+
+	/**
+	 * If this page configurator is enabled
+	 *
+	 * @return if the configuration must run
+	 */
+	@Override
+	public boolean enabled()
+	{
+		return MomentPageConfigurator.isEnabled();
+	}
 }
