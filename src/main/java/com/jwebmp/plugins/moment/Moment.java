532c72048f551569f8f1ac5cc48b4d536156aadb
GedMarc <ged_marc@hotmail.com>
2019-01-17 22:01:51 +0200

    GPG Update

--- src/main/java/com/jwebmp/plugins/moment/Moment.java
+++ src/main/java/com/jwebmp/plugins/moment/Moment.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 Marc Magon
+ * Copyright (C) 2017 GedMarc
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -36,7 +36,7 @@ import static com.jwebmp.core.utilities.StaticStrings.*;
  *
  * @param <J>
  *
- * @author Marc Magon
+ * @author GedMarc
  * @version 1.0
  * @since 29 Aug 2015
  */
