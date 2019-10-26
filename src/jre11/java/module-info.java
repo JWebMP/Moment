5a9d7e8b01110f530a5a9ab7564c2431f6ba41b1
GedMarc <ged_marc@hotmail.com>
2019-10-26 02:35:18 +0200

    Guiced EE Migration to its own space

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -3,18 +3,18 @@ module com.jwebmp.plugins.moment {
 	exports com.jwebmp.plugins.moment;
 
 	requires com.jwebmp.core;
-	requires com.jwebmp.logmaster;
+	requires com.guicedee.logmaster;
 	requires com.fasterxml.jackson.annotation;
 
 	requires java.validation;
 	requires java.logging;
-	requires com.jwebmp.guicedinjection;
+	requires com.guicedee.guicedinjection;
 	requires com.jwebmp.core.angularjs;
 
 	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.moment.MomentPageConfigurator;
 
-	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.moment.implementations.MomentExclusionsModule;
-	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.moment.implementations.MomentExclusionsModule;
+	provides com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.moment.implementations.MomentExclusionsModule;
+	provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.moment.implementations.MomentExclusionsModule;
 
 	provides com.jwebmp.core.base.angular.services.IAngularModule with com.jwebmp.plugins.moment.implementations.MomentAngularModule;
 
