57ae49b208922bca71beb46384d38198813e6da4
GedMarc <ged_marc@hotmial.com>
2020-03-08 16:59:57 +0200

    Remove Jar syntax as JDK 8 is no longer supported

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -13,7 +13,6 @@ module com.jwebmp.plugins.moment {
 
 	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.moment.MomentPageConfigurator;
 
-	provides com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.moment.implementations.MomentExclusionsModule;
 	provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.moment.implementations.MomentExclusionsModule;
 
 	provides com.jwebmp.core.base.angular.services.IAngularModule with com.jwebmp.plugins.moment.implementations.MomentAngularModule;
--- src/main/java/com/jwebmp/plugins/moment/implementations/MomentExclusionsModule.java
+++ src/main/java/com/jwebmp/plugins/moment/implementations/MomentExclusionsModule.java
@@ -1,6 +1,5 @@
 package com.jwebmp.plugins.moment.implementations;
 
-import com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions;
 import com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions;
 
 import javax.validation.constraints.NotNull;
@@ -8,18 +7,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class MomentExclusionsModule
-		implements IGuiceScanModuleExclusions<MomentExclusionsModule>,
-				           IGuiceScanJarExclusions<MomentExclusionsModule>
+		implements IGuiceScanModuleExclusions<MomentExclusionsModule>
 {
-
-	@Override
-	public @NotNull Set<String> excludeJars()
-	{
-		Set<String> strings = new HashSet<>();
-		strings.add("jwebmp-momentjs-*");
-		return strings;
-	}
-
 	@Override
 	public @NotNull Set<String> excludeModules()
 	{
