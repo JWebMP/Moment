81eca19efe1aa5811466db2d7b0575504241e44b
GedMarc <marc.magon@gmail.com>
2024-03-02 20:27:29 +0200

    Add Moment

--- src/main/java/com/jwebmp/plugins/moment/MomentPageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/moment/MomentPageConfigurator.java
@@ -27,78 +27,77 @@ import jakarta.validation.constraints.*;
  * @since 22 Feb 2017
  */
 @PluginInformation(pluginName = "Moment.js",
-		pluginUniqueName = "moment-datetime",
-		pluginDescription =
-				"Moment.js provides a wrapper for the native JavaScript date object. In doing this, Moment.js extends the functionality and also accounts for several deficiencies in the object.\n" +
-				"<br/>" +
-				"Parsing is notably unpredictable with native date. For instance, suppose I am using a computer in the United States, but I have a date in DD/MM/YYYY format.",
-		pluginVersion = "2.17.1",
-		pluginDependancyUniqueIDs = "jquery,angular",
-		pluginCategories = "bootstrap,web ui,ui,framework,date picker,date time, moment",
-		pluginSubtitle = "Parse, validate, manipulate, and display dates in JavaScript.",
-		pluginGitUrl = "https://github.com/GedMarc/JWebMP-MomentPlugin",
-		pluginSourceUrl = "https://momentjs.com/",
-		pluginOriginalHomepage = "https://momentjs.com",
-		pluginWikiUrl = "https://github.com/GedMarc/JWebMP-MomentPlugin/wiki",
-		pluginDownloadUrl = "https://mvnrepository.com/artifact/com.jwebmp.plugins.javascript/jwebmp-momentjs",
-		pluginIconUrl = "bower_components/moment/moment_icon.png",
-		pluginIconImageUrl = "bower_components/moment/moment_logo.png",
-		pluginLastUpdatedDate = "2017/03/04",
-		pluginGroupId = "com.jwebmp.plugins.javascript",
-		pluginArtifactId = "jwebmp-momentjs",
-		pluginModuleName = "com.jwebmp.plugins.moment",
-		pluginStatus = PluginStatus.Released
+        pluginUniqueName = "moment-datetime",
+        pluginDescription =
+                "Moment.js provides a wrapper for the native JavaScript date object. In doing this, Moment.js extends the functionality and also accounts for several deficiencies in the object.\n" +
+                        "<br/>" +
+                        "Parsing is notably unpredictable with native date. For instance, suppose I am using a computer in the United States, but I have a date in DD/MM/YYYY format.",
+        pluginVersion = "2.17.1",
+        pluginDependancyUniqueIDs = "jquery,angular",
+        pluginCategories = "bootstrap,web ui,ui,framework,date picker,date time, moment",
+        pluginSubtitle = "Parse, validate, manipulate, and display dates in JavaScript.",
+        pluginGitUrl = "https://github.com/GedMarc/JWebMP-MomentPlugin",
+        pluginSourceUrl = "https://momentjs.com/",
+        pluginOriginalHomepage = "https://momentjs.com",
+        pluginWikiUrl = "https://github.com/GedMarc/JWebMP-MomentPlugin/wiki",
+        pluginDownloadUrl = "https://mvnrepository.com/artifact/com.jwebmp.plugins.javascript/jwebmp-momentjs",
+        pluginIconUrl = "bower_components/moment/moment_icon.png",
+        pluginIconImageUrl = "bower_components/moment/moment_logo.png",
+        pluginLastUpdatedDate = "2017/03/04",
+        pluginGroupId = "com.jwebmp.plugins.javascript",
+        pluginArtifactId = "jwebmp-momentjs",
+        pluginModuleName = "com.jwebmp.plugins.moment",
+        pluginStatus = PluginStatus.Released
 )
 @TsDependency(value = "moment", version = "^2.29.3")
 public class MomentPageConfigurator
-		implements IPageConfigurator<MomentPageConfigurator>
+        implements IPageConfigurator<MomentPageConfigurator>
 {
-	/**
-	 * If this configurator is enabled
-	 */
-	private static boolean enabled = true;
+    /**
+     * If this configurator is enabled
+     */
+    private static boolean enabled = true;
 
-	public MomentPageConfigurator()
-	{
-		//Nothing Needed
-	}
+    public MomentPageConfigurator()
+    {
+        //Nothing Needed
+    }
 
-	/**
-	 * Method isEnabled returns the enabled of this AngularAnimatedChangePageConfigurator object.
-	 * <p>
-	 * If this configurator is enabled
-	 *
-	 * @return the enabled (type boolean) of this AngularAnimatedChangePageConfigurator object.
-	 */
-	public static boolean isEnabled()
-	{
-		return MomentPageConfigurator.enabled;
-	}
+    /**
+     * Method isEnabled returns the enabled of this AngularAnimatedChangePageConfigurator object.
+     * <p>
+     * If this configurator is enabled
+     *
+     * @return the enabled (type boolean) of this AngularAnimatedChangePageConfigurator object.
+     */
+    public static boolean isEnabled()
+    {
+        return MomentPageConfigurator.enabled;
+    }
 
-	/**
-	 * Method setEnabled sets the enabled of this AngularAnimatedChangePageConfigurator object.
-	 * <p>
-	 * If this configurator is enabled
-	 *
-	 * @param mustEnable
-	 * 		the enabled of this AngularAnimatedChangePageConfigurator object.
-	 */
-	public static void setEnabled(boolean mustEnable)
-	{
-		MomentPageConfigurator.enabled = mustEnable;
-	}
+    /**
+     * Method setEnabled sets the enabled of this AngularAnimatedChangePageConfigurator object.
+     * <p>
+     * If this configurator is enabled
+     *
+     * @param mustEnable the enabled of this AngularAnimatedChangePageConfigurator object.
+     */
+    public static void setEnabled(boolean mustEnable)
+    {
+        MomentPageConfigurator.enabled = mustEnable;
+    }
 
-	@NotNull
-	@Override
-	@SuppressWarnings("unchecked")
-	 public Page<?> configure(Page<?> page)
-	{
-		return page;
-	}
+    @NotNull
+    @Override
+    @SuppressWarnings("unchecked")
+    public IPage<?> configure(IPage<?> page)
+    {
+        return page;
+    }
 
-	@Override
-	public boolean enabled()
-	{
-		return MomentPageConfigurator.enabled;
-	}
+    @Override
+    public boolean enabled()
+    {
+        return MomentPageConfigurator.enabled;
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,14 +2,16 @@ import com.jwebmp.plugins.moment.implementations.MomentIncludeModule;
 
 module com.jwebmp.plugins.moment {
 
-	exports com.jwebmp.plugins.moment;
-	
-	requires com.jwebmp.core.base.angular.client;
-	requires com.guicedee.jsonrepresentation;
-	
-	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.moment.MomentPageConfigurator;
-	provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleInclusions with MomentIncludeModule;
+    exports com.jwebmp.plugins.moment;
+    requires com.jwebmp.client;
+    requires com.jwebmp.core;
 
-	opens com.jwebmp.plugins.moment to com.fasterxml.jackson.databind, com.jwebmp.core, com.google.guice;
-	opens com.jwebmp.plugins.moment.implementations to com.fasterxml.jackson.databind, com.jwebmp.core, com.google.guice;
+    requires com.jwebmp.core.base.angular.client;
+    requires com.guicedee.jsonrepresentation;
+
+    provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.moment.MomentPageConfigurator;
+    provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleInclusions with MomentIncludeModule;
+
+    opens com.jwebmp.plugins.moment to com.fasterxml.jackson.databind, com.jwebmp.core, com.google.guice;
+    opens com.jwebmp.plugins.moment.implementations to com.fasterxml.jackson.databind, com.jwebmp.core, com.google.guice;
 }
